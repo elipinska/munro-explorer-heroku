3608d5394de23d97bbf0139f4fc17b8e6a4a770a
elipinska <ewa.lipinska@hotmail.com>
2018-04-06 01:49:00 +0100

    Fix db problems

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: ENV['DATABASE_URL'], host: 'ec2-54-163-246-193.compute-1.amazonaws.com' })
+      db = PG.connect({ dbname: dbbuohk48l5i16, host: 'ec2-54-163-246-193.compute-1.amazonaws.com' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
