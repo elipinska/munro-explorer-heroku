523238985647a8a847378f07d8eec8d989cd3d16
elipinska <ewa.lipinska@hotmail.com>
2018-04-06 01:43:27 +0100

    Fix db problems

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'munro_bagger', host: 'localhost' })
+      db = PG.connect({ dbname: ENV['DATABASE_URL'], host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
