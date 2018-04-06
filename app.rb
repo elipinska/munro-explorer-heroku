7baf8c35e6af33fce9fd365681791b7229a59ed3
elipinska <ewa.lipinska@hotmail.com>
2018-04-06 02:51:16 +0100

    Removing unnecessary gem links

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require 'sinatra/activerecord'
 require_relative('controllers/hike_controller')
 require_relative('controllers/hiker_controller')
 require_relative('controllers/munro_controller')
-require 'active_record'
 
 
   get '/' do
