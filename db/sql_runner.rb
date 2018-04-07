1cc905dd880222bacc46339f488ca006c3c6ab5a
elipinska <ewa.lipinska@hotmail.com>
2018-04-07 11:26:01 +0100

    Edit buttons on hikes index

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db_info = ENV['DATABASE_URL'].nil? ? {dbname: 'munro-bagger', host: 'localhost'} : ENV['DATABASE_URL']
+      db_info = ENV['DATABASE_URL'].nil? ? {dbname: 'munro_bagger', host: 'localhost'} : ENV['DATABASE_URL']
       db = PG.connect(db_info)
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
